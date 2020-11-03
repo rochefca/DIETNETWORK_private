9f1efb85d75ab320ac9b20ee66ea6f58e101573a
Matthew Scicluna <sciclun4@beluga3.int.ets1.calculquebec.ca>
2020-11-03 11:02:54 -0500

    changed experiment scripts and added some documentation

--- Dietnet/Interpretability/experiment_scripts/run_snp_removal_experiment.sh
+++ Dietnet/Interpretability/experiment_scripts/run_snp_removal_experiment.sh
@@ -5,40 +5,42 @@
 rounds=( 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 )
 folds=( 0 1 2 3 4 )
 
+cp run_snp_removal_experiment.sh ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train
+
 #  zeroth iteration is original dataset and embedding (i.e. we did not remove any SNPs yet)
-cp ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds/dataset.npz ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds/dataset-0.npz
-cp ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds/embedding.npz ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds/embedding-0.npz
+cp ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train/dataset.npz ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train/dataset-0.npz
+cp ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train/embedding.npz ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train/embedding-0.npz
 
 wait
 
-echo sbatch --parsable sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds \
+echo sbatch --parsable sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train \
                                                   --exp-name "remove_attr_exp_0" \
                                                   --dataset "dataset-0.npz" \
                                                   --embedding "embedding-0.npz" \
-                                                  --epochs 4000 \
+                                                  --epochs 20000 \
                                                   --patience 2000
 
-train_id=$(sbatch --parsable sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds \
+train_id=$(sbatch --parsable sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train \
                                                         --exp-name "remove_attr_exp_0" \
                                                         --dataset "dataset-0.npz" \
                                                         --embedding "embedding-0.npz" \
-                                                        --epochs 4000 \
-                                                        --patience 200)
+                                                        --epochs 20000 \
+                                                        --patience 2000)
 
 for i in ${rounds[@]}; do
     j=`expr $i + 1`
 
-    echo sbatch --parsable --depend=afterok:${train_id} sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds  \
-                                                                                         --exp-name "remove_attr_exp_${i}" \
-                                                                                         --genotypes "dataset-${i}.npz" \
-                                                                                         --dataset "dataset-${i}.npz" \
-                                                                                         --embedding "embedding-${i}.npz" \
-                                                                                         --percentile-to-remove 85 \
-                                                                                         --dataset-out "dataset-${j}.npz" \
-                                                                                         --embedding-out "embedding-${j}.npz" \
-                                                                                         --which-fold 0 1 2 3 4
+    echo sbatch --parsable --depend=afterok:${train_id} sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train  \
+                                                                                      --exp-name "remove_attr_exp_${i}" \
+                                                                                      --genotypes "dataset-${i}.npz" \
+                                                                                      --dataset "dataset-${i}.npz" \
+                                                                                      --embedding "embedding-${i}.npz" \
+                                                                                      --percentile-to-remove 85 \
+                                                                                      --dataset-out "dataset-${j}.npz" \
+                                                                                      --embedding-out "embedding-${j}.npz" \
+                                                                                      --which-fold 0 1 2 3 4
 
-    rm_id=$(sbatch --parsable --depend=afterok:${train_id} sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds  \
+    rm_id=$(sbatch --parsable --depend=afterok:${train_id} sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train  \
                                                                                          --exp-name "remove_attr_exp_${i}" \
                                                                                          --genotypes "dataset-${i}.npz" \
                                                                                          --dataset "dataset-${i}.npz" \
@@ -50,18 +52,18 @@ for i in ${rounds[@]}; do
 
     wait
 
-    echo sbatch --parsable --depend=afterok:${rm_id} sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds \
+    echo sbatch --parsable --depend=afterok:${rm_id} sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train \
                                                                                 --exp-name "remove_attr_exp_${j}" \
                                                                                 --dataset "dataset-${j}.npz" \
                                                                                 --embedding "embedding-${j}.npz" \
-                                                                                --epochs 4000 \
+                                                                                --epochs 20000 \
                                                                                 --patience 2000
 
-    train_id=$(sbatch --parsable --depend=afterok:${rm_id} sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds \
+    train_id=$(sbatch --parsable --depend=afterok:${rm_id} sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train \
                                                                                       --exp-name "remove_attr_exp_${j}" \
                                                                                       --dataset "dataset-${j}.npz" \
                                                                                       --embedding "embedding-${j}.npz" \
-                                                                                      --epochs 4000 \
+                                                                                      --epochs 20000 \
                                                                                       --patience 2000)
 
     wait
--- Dietnet/Interpretability/experiment_scripts/run_snp_removal_experiment_baseline.sh
+++ Dietnet/Interpretability/experiment_scripts/run_snp_removal_experiment_baseline.sh
@@ -2,10 +2,14 @@
 #export SLURM_ACCOUNT="def-hussinju"
 #export SBATCH_ACCOUNT=$SLURM_ACCOUNT
 
+# copy over this file for future reference
+cp run_snp_removal_experiment_baseline.sh ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train
+
 rounds=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 )
 folds=( 0 1 2 3 4 )
 
-echo sbatch --parsable sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds  \
+# remove SNPs randomly (starting with all SNPs)
+echo sbatch --parsable sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train  \
                                                      --exp-name "remove_attr_exp_0" \
                                                      --genotypes "dataset-0.npz" \
                                                      --dataset "dataset-0.npz" \
@@ -16,7 +20,7 @@ echo sbatch --parsable sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_r
                                                      --embedding-out "embedding-1-baseline.npz" \
                                                      --which-fold 0 1 2 3 4
 
-rm_id=$(sbatch --parsable sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds  \
+rm_id=$(sbatch --parsable sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train  \
                                                         --exp-name "remove_attr_exp_0" \
                                                         --genotypes "dataset-0.npz" \
                                                         --dataset "dataset-0.npz" \
@@ -29,24 +33,28 @@ rm_id=$(sbatch --parsable sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/ex
 
 for i in ${rounds[@]}; do
     j=`expr $i + 1`
-
-    echo sbatch --parsable --depend=afterok:${rm_id} sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds \
+    
+    # train networks on dataset and embedding generated from round i of random SNP removal
+    echo sbatch --parsable --depend=afterok:${rm_id} sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train \
                                                                                 --exp-name "remove_attr_exp_${i}_baseline" \
                                                                                 --dataset "dataset-${i}-baseline.npz" \
                                                                                 --embedding "embedding-${i}-baseline.npz" \
-                                                                                --epochs 4000 \
+                                                                                --epochs 20000 \
                                                                                 --patience 2000
 
-    train_id=$(sbatch --parsable --depend=afterok:${rm_id} sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds \
+    train_id=$(sbatch --parsable --depend=afterok:${rm_id} sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train \
                                                                                       --exp-name "remove_attr_exp_${i}_baseline" \
                                                                                       --dataset "dataset-${i}-baseline.npz" \
                                                                                       --embedding "embedding-${i}-baseline.npz" \
-                                                                                      --epochs 4000 \
+                                                                                      --epochs 20000 \
                                                                                       --patience 2000)
 
     wait
 
-    echo sbatch --parsable --depend=afterok:${train_id} sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds  \
+    # generate dataset and embedding generated for round j=i+1 of random SNP removal.
+    # we remove random 15 percentag of SNPs from dataset-i.npz (recall this was produced during round i-1 of NON-RANDOM SNP removal)
+    # see run_snp_removal_experiment.sh script for details of NON-RANDOM SNP removal
+    echo sbatch --parsable --depend=afterok:${train_id} sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train  \
                                                                                       --exp-name "remove_attr_exp_${i}" \
                                                                                       --genotypes "dataset-${i}.npz" \
                                                                                       --dataset "dataset-${i}.npz" \
@@ -57,7 +65,7 @@ for i in ${rounds[@]}; do
                                                                                       --embedding-out "embedding-${j}-baseline.npz" \
                                                                                       --which-fold 0 1 2 3 4
 
-    rm_id=$(sbatch --parsable --depend=afterok:${train_id} sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds  \
+    rm_id=$(sbatch --parsable --depend=afterok:${train_id} sbatch_wrapper_snp_removal.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train  \
                                                                                          --exp-name "remove_attr_exp_${i}" \
                                                                                          --genotypes "dataset-${i}.npz" \
                                                                                          --dataset "dataset-${i}.npz" \
@@ -73,16 +81,17 @@ for i in ${rounds[@]}; do
     echo "submitted round ${i} of SNP reduction"
 done
 
-echo sbatch --parsable --depend=afterok:${rm_id} sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds \
+# train networks on dataset and embedding generated from final round of random SNP removal
+echo sbatch --parsable --depend=afterok:${rm_id} sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train \
                                                                             --exp-name "remove_attr_exp_21_baseline" \
                                                                             --dataset "dataset-21-baseline.npz" \
                                                                             --embedding "embedding-21-baseline.npz" \
-                                                                            --epochs 4000 \
+                                                                            --epochs 20000 \
                                                                             --patience 2000
 
-train_id=$(sbatch --parsable --depend=afterok:${rm_id} sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_enc_0_baseline_all_folds \
+train_id=$(sbatch --parsable --depend=afterok:${rm_id} sbatch_wrapper_allfolds.sh --exp-path ${SCRATCH}/exp_results/DIETNETWORK/1000G/remove_largest_attr_long_train \
                                                                                   --exp-name "remove_attr_exp_21_baseline" \
                                                                                   --dataset "dataset-21-baseline.npz" \
                                                                                   --embedding "embedding-21-baseline.npz" \
-                                                                                  --epochs 4000 \
+                                                                                  --epochs 20000 \
                                                                                   --patience 2000)
