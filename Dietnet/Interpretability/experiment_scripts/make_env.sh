d6966c78d188e71afdd297e4e3e576ff7e95ebf6
Matthew Scicluna <mattcscicluna@gmail.com>
2020-11-14 11:31:17 -0500

    removing bash files from experimental scripts

--- Dietnet/Interpretability/experiment_scripts/make_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# do NOT run in interactive mode
-module load python/3.6
-virtualenv --no-download dietnetwork
-
-source dietnetwork/bin/activate
-pip install --no-index --upgrade pip
-pip install --no-index -r /lustre03/project/6009524/cam27/DIETNET_EXP/MATTHEW_DATA/DietnetEnv_requirements.txt
